e3c5cc2ac85414b079ab77e5f42a941863aa8fc6
Nguyen Xuan Bac <ngxbac.dt@gmail.com>
2019-05-31 20:50:45 +0900

    train target

--- preprocessing/extract_data.py
+++ preprocessing/extract_data.py
@@ -3,6 +3,8 @@ import pandas as pd
 import click
 import os
 from tqdm import *
+import pickle
+from sklearn.preprocessing import StandardScaler
 from pytorch_pretrained_bert import BertTokenizer
 
 
@@ -47,6 +49,115 @@ def extract_data(
 
     sequences = convert_lines(df["comment_text"].fillna("DUMMY_VALUE"), max_sequence_length, tokenizer)
     np.save(os.path.join(output_path, f'sequence_{dataset}.npy'), sequences)
+
+
+
+symbols_to_isolate = '.,?!-;*"…:—()%#$&_/@＼・ω+=”“[]^–>\\°<~•≠™ˈʊɒ∞§{}·τα❤☺ɡ|¢→̶`❥━┣┫┗Ｏ►★©―ɪ✔®\x96\x92●£♥➤´¹☕≈÷♡◐║▬′ɔː€۩۞†μ✒➥═☆ˌ◄½ʻπδηλσερνʃ✬ＳＵＰＥＲＩＴ☻±♍µº¾✓◾؟．⬅℅»Вав❣⋅¿¬♫ＣＭβ█▓▒░⇒⭐›¡₂₃❧▰▔◞▀▂▃▄▅▆▇↙γ̄″☹➡«φ⅓„✋：¥̲̅́∙‛◇✏▷❓❗¶˚˙）сиʿ✨。ɑ\x80◕！％¯−ﬂﬁ₁²ʌ¼⁴⁄₄⌠♭✘╪▶☭✭♪☔☠♂☃☎✈✌✰❆☙○‣⚓年∎ℒ▪▙☏⅛ｃａｓǀ℮¸ｗ‚∼‖ℳ❄←☼⋆ʒ⊂、⅔¨͡๏⚾⚽Φ×θ￦？（℃⏩☮⚠月✊❌⭕▸■⇌☐☑⚡☄ǫ╭∩╮，例＞ʕɐ̣Δ₀✞┈╱╲▏▕┃╰▊▋╯┳┊≥☒↑☝ɹ✅☛♩☞ＡＪＢ◔◡↓♀⬆̱ℏ\x91⠀ˤ╚↺⇤∏✾◦♬³の｜／∵∴√Ω¤☜▲↳▫‿⬇✧ｏｖｍ－２０８＇‰≤∕ˆ⚜☁'
+symbols_to_delete = '\n🍕\r🐵😑\xa0\ue014\t\uf818\uf04a\xad😢🐶️\uf0e0😜😎👊\u200b\u200e😁عدويهصقأناخلىبمغر😍💖💵Е👎😀😂\u202a\u202c🔥😄🏻💥ᴍʏʀᴇɴᴅᴏᴀᴋʜᴜʟᴛᴄᴘʙғᴊᴡɢ😋👏שלוםבי😱‼\x81エンジ故障\u2009🚌ᴵ͞🌟😊😳😧🙀😐😕\u200f👍😮😃😘אעכח💩💯⛽🚄🏼ஜ😖ᴠ🚲‐😟😈💪🙏🎯🌹😇💔😡\x7f👌ἐὶήιὲκἀίῃἴξ🙄Ｈ😠\ufeff\u2028😉😤⛺🙂\u3000تحكسة👮💙فزط😏🍾🎉😞\u2008🏾😅😭👻😥😔😓🏽🎆🍻🍽🎶🌺🤔😪\x08‑🐰🐇🐱🙆😨🙃💕𝘊𝘦𝘳𝘢𝘵𝘰𝘤𝘺𝘴𝘪𝘧𝘮𝘣💗💚地獄谷улкнПоАН🐾🐕😆ה🔗🚽歌舞伎🙈😴🏿🤗🇺🇸мυтѕ⤵🏆🎃😩\u200a🌠🐟💫💰💎эпрд\x95🖐🙅⛲🍰🤐👆🙌\u2002💛🙁👀🙊🙉\u2004ˢᵒʳʸᴼᴷᴺʷᵗʰᵉᵘ\x13🚬🤓\ue602😵άοόςέὸתמדףנרךצט😒͝🆕👅👥👄🔄🔤👉👤👶👲🔛🎓\uf0b7\uf04c\x9f\x10成都😣⏺😌🤑🌏😯ех😲Ἰᾶὁ💞🚓🔔📚🏀👐\u202d💤🍇\ue613小土豆🏡❔⁉\u202f👠》कर्मा🇹🇼🌸蔡英文🌞🎲レクサス😛外国人关系Сб💋💀🎄💜🤢َِьыгя不是\x9c\x9d🗑\u2005💃📣👿༼つ༽😰ḷЗз▱ц￼🤣卖温哥华议会下降你失去所有的钱加拿大坏税骗子🐝ツ🎅\x85🍺آإشء🎵🌎͟ἔ油别克🤡🤥😬🤧й\u2003🚀🤴ʲшчИОРФДЯМюж😝🖑ὐύύ特殊作戦群щ💨圆明园קℐ🏈😺🌍⏏ệ🍔🐮🍁🍆🍑🌮🌯🤦\u200d𝓒𝓲𝓿𝓵안영하세요ЖљКћ🍀😫🤤ῦ我出生在了可以说普通话汉语好极🎼🕺🍸🥂🗽🎇🎊🆘🤠👩🖒🚪天一家⚲\u2006⚭⚆⬭⬯⏖新✀╌🇫🇷🇩🇪🇮🇬🇧😷🇨🇦ХШ🌐\x1f杀鸡给猴看ʁ𝗪𝗵𝗲𝗻𝘆𝗼𝘂𝗿𝗮𝗹𝗶𝘇𝗯𝘁𝗰𝘀𝘅𝗽𝘄𝗱📺ϖ\u2000үսᴦᎥһͺ\u2007հ\u2001ɩｙｅ൦ｌƽｈ𝐓𝐡𝐞𝐫𝐮𝐝𝐚𝐃𝐜𝐩𝐭𝐢𝐨𝐧Ƅᴨןᑯ໐ΤᏧ௦Іᴑ܁𝐬𝐰𝐲𝐛𝐦𝐯𝐑𝐙𝐣𝐇𝐂𝐘𝟎ԜТᗞ౦〔Ꭻ𝐳𝐔𝐱𝟔𝟓𝐅🐋ﬃ💘💓ё𝘥𝘯𝘶💐🌋🌄🌅𝙬𝙖𝙨𝙤𝙣𝙡𝙮𝙘𝙠𝙚𝙙𝙜𝙧𝙥𝙩𝙪𝙗𝙞𝙝𝙛👺🐷ℋ𝐀𝐥𝐪🚶𝙢Ἱ🤘ͦ💸ج패티Ｗ𝙇ᵻ👂👃ɜ🎫\uf0a7БУі🚢🚂ગુજરાતીῆ🏃𝓬𝓻𝓴𝓮𝓽𝓼☘﴾̯﴿₽\ue807𝑻𝒆𝒍𝒕𝒉𝒓𝒖𝒂𝒏𝒅𝒔𝒎𝒗𝒊👽😙\u200cЛ‒🎾👹⎌🏒⛸公寓养宠物吗🏄🐀🚑🤷操美𝒑𝒚𝒐𝑴🤙🐒欢迎来到阿拉斯ספ𝙫🐈𝒌𝙊𝙭𝙆𝙋𝙍𝘼𝙅ﷻ🦄巨收赢得白鬼愤怒要买额ẽ🚗🐳𝟏𝐟𝟖𝟑𝟕𝒄𝟗𝐠𝙄𝙃👇锟斤拷𝗢𝟳𝟱𝟬⦁マルハニチロ株式社⛷한국어ㄸㅓ니͜ʖ𝘿𝙔₵𝒩ℯ𝒾𝓁𝒶𝓉𝓇𝓊𝓃𝓈𝓅ℴ𝒻𝒽𝓀𝓌𝒸𝓎𝙏ζ𝙟𝘃𝗺𝟮𝟭𝟯𝟲👋🦊多伦🐽🎻🎹⛓🏹🍷🦆为和中友谊祝贺与其想象对法如直接问用自己猜本传教士没积唯认识基督徒曾经让相信耶稣复活死怪他但当们聊些政治题时候战胜因圣把全堂结婚孩恐惧且栗谓这样还♾🎸🤕🤒⛑🎁批判检讨🏝🦁🙋😶쥐스탱트뤼도석유가격인상이경제황을렵게만들지않록잘관리해야합다캐나에서대마초와화약금의품런성분갈때는반드시허된사용🔫👁凸ὰ💲🗯𝙈Ἄ𝒇𝒈𝒘𝒃𝑬𝑶𝕾𝖙𝖗𝖆𝖎𝖌𝖍𝖕𝖊𝖔𝖑𝖉𝖓𝖐𝖜𝖞𝖚𝖇𝕿𝖘𝖄𝖛𝖒𝖋𝖂𝕴𝖟𝖈𝕸👑🚿💡知彼百\uf005𝙀𝒛𝑲𝑳𝑾𝒋𝟒😦𝙒𝘾𝘽🏐𝘩𝘨ὼṑ𝑱𝑹𝑫𝑵𝑪🇰🇵👾ᓇᒧᔭᐃᐧᐦᑳᐨᓃᓂᑲᐸᑭᑎᓀᐣ🐄🎈🔨🐎🤞🐸💟🎰🌝🛳点击查版🍭𝑥𝑦𝑧ＮＧ👣\uf020っ🏉ф💭🎥Ξ🐴👨🤳🦍\x0b🍩𝑯𝒒😗𝟐🏂👳🍗🕉🐲چی𝑮𝗕𝗴🍒ꜥⲣⲏ🐑⏰鉄リ事件ї💊「」\uf203\uf09a\uf222\ue608\uf202\uf099\uf469\ue607\uf410\ue600燻製シ虚偽屁理屈Г𝑩𝑰𝒀𝑺🌤𝗳𝗜𝗙𝗦𝗧🍊ὺἈἡχῖΛ⤏🇳𝒙ψՁմեռայինրւդձ冬至ὀ𝒁🔹🤚🍎𝑷🐂💅𝘬𝘱𝘸𝘷𝘐𝘭𝘓𝘖𝘹𝘲𝘫کΒώ💢ΜΟΝΑΕ🇱♲𝝈↴💒⊘Ȼ🚴🖕🖤🥘📍👈➕🚫🎨🌑🐻𝐎𝐍𝐊𝑭🤖🎎😼🕷ｇｒｎｔｉｄｕｆｂｋ𝟰🇴🇭🇻🇲𝗞𝗭𝗘𝗤👼📉🍟🍦🌈🔭《🐊🐍\uf10aლڡ🐦\U0001f92f\U0001f92a🐡💳ἱ🙇𝗸𝗟𝗠𝗷🥜さようなら🔼'
+good_emoji = [':)', ':-)', ';-)', ';)']
+spaces = ['\u2028', '\u2061', '\u202c', '\u200d', '\u200f', '\x9d', '\u2005', '\u202a', '\u2009', '\u2004',
+          '\x10', '\u200b', '\u200c', '\xad', '\u202f', '\u200e', '\ufeff', '\uf0d8', '\x7f', '\xa0', '\u200a', '\r',
+          '\u2007', '\u2002', '\u3000', '\u2006', '\u2003']
+
+
+def remove_spaces(x):
+    for sp in spaces:
+        x = x.replace(sp, ' ')
+    return x
+
+
+def add_features(df):
+    df['total_length'] = df['comment_text'].apply(len)
+    df['capitals'] = df['comment_text'].apply(lambda comment: sum(1 for c in comment if c.isupper()))
+    df['caps_vs_length'] = df.apply(lambda row: float(row['capitals']) / float(row['total_length']), axis=1)
+    df['num_exclamation_marks'] = df['comment_text'].apply(lambda comment: comment.count('!'))
+    df['num_question_marks'] = df['comment_text'].apply(lambda comment: comment.count('?'))
+    df['num_punctuation'] = df['comment_text'].apply(lambda comment: sum(comment.count(w) for w in '.,;:'))
+    df['num_symbols'] = df['comment_text'].apply(lambda comment: sum(comment.count(w) for w in '*&$%'))
+    df['num_words'] = df['comment_text'].apply(lambda comment: len(comment.split()))
+    df['num_unique_words'] = df['comment_text'].apply(lambda comment: len(set(w for w in comment.split())))
+    df['words_vs_unique'] = df['num_unique_words'] / df['num_words']
+    df['num_smilies'] = df['comment_text'].apply(lambda comment: sum(comment.count(w) for w in good_emoji))
+    return df
+
+
+@cli.command()
+@click.option('--model_path', type=str)
+@click.option('--csv_file', type=str)
+@click.option('--dataset', type=str)
+@click.option('--max_sequence_length', type=int)
+@click.option('--output_path', type=str)
+def meta_features(
+    csv_file,
+    dataset,
+    output_path,
+):
+    os.makedirs(output_path, exist_ok=True)
+    df = pd.read_csv(csv_file)
+
+    df['comment_text'] = df['comment_text'].astype(str).apply(lambda x: remove_spaces(x))
+    df = add_features(df)
+
+    feature_cols = [
+        'total_length', 'capitals', 'caps_vs_length', 'num_exclamation_marks', 'num_question_marks',
+        'num_punctuation', 'num_words', 'num_unique_words', 'words_vs_unique', 'num_smilies', 'num_symbols'
+    ]
+    features = df[feature_cols]
+    features.fillna(0, inplace=True)
+
+    if dataset == 'train':
+        ss = StandardScaler()
+        ss.fit(features)
+    else:
+        with open(os.path.join(output_path, f'ss_train.pkl')) as f:
+            ss = pickle.load(f)
+
+    features = np.array(ss.transform(features), dtype=np.float32)
+    if dataset == 'train':
+        with open(os.path.join(output_path, f'ss_{dataset}.pkl')) as f:
+            pickle.dump(ss, f)
+    np.save(os.path.join(output_path, f'meta_features_{dataset}.npy'), features)
+
+
+
+@cli.command()
+@click.option('--csv_file', type=str)
+@click.option('--output_path', type=str)
+def train_target(
+    csv_file,
+    output_path,
+):
+    os.makedirs(output_path, exist_ok=True)
+    df = pd.read_csv(csv_file)
+
+    aux_columns = ['target', 'severe_toxicity', 'obscene', 'identity_attack', 'insult', 'threat']
+
+    N_AUX_TARGETS = len(aux_columns)
+
+    identity_columns = [
+        'male', 'female', 'homosexual_gay_or_lesbian', 'christian', 'jewish',
+        'muslim', 'black', 'white', 'psychiatric_or_mental_illness'
+    ]
+
+    weights = np.ones((len(df),)) / 4
+    # Subgroup
+    weights += (df[identity_columns].fillna(0).values >= 0.5).sum(axis=1).astype(bool).astype(np.int) / 4
+    # Background Positive, Subgroup Negative
+    weights += (((df['target'].values >= 0.5).astype(bool).astype(np.int) +
+                 (df[identity_columns].fillna(0).values < 0.5).sum(axis=1).astype(bool).astype(
+                     np.int)) > 1).astype(bool).astype(np.int) / 4
+    # Background Negative, Subgroup Positive
+    weights += (((df['target'].values < 0.5).astype(bool).astype(np.int) +
+                 (df[identity_columns].fillna(0).values >= 0.5).sum(axis=1).astype(bool).astype(
+                     np.int)) > 1).astype(bool).astype(np.int) / 4
+    loss_weight = 1.0 / weights.mean()
+
+    y_train_aux = df[aux_columns].values
+    train_y_target = (df['target'].values > 0.5).astype(int)
+    y_train = np.vstack([train_y_target, weights]).T
+    np.save(os.path.join(output_path, f'y_train_aux.npy'), y_train_aux)
+    np.save(os.path.join(output_path, f'y_train.npy'), y_train)
+    np.save(os.path.join(output_path, f'loss_weight.npy'), loss_weight)
 
 
 if __name__ == '__main__':
